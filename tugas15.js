6930150243aa76b1b49540541ef986d60c5fc603
heriksuseno <heriksuseno21@mail.com>
2021-02-23 22:43:53 +0700

    Tugas 15 JS untuk NIOMIC

---
+++ tugas15.js
@@ -0,0 +1,9 @@
+function segitigasikusiku() {
+  var x = 8;
+  var y = 6;
+  console.log('Nilai AB :', x)
+  console.log('Nilai BC :', y)
+  console.log('Panjang sisi AC pada segitiga siku-siku tersebut adalah :', Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)),'Cm');
+
+}
+segitigasikusiku()
